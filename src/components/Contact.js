63302320fa584805cb3675907f3a7d3846425d94
TheCats100 <m-g-152@outlook.fr>
2020-03-30 11:51:27 +0200

    exoDeuxTerminé

---
+++ src/components/Contact.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Css.css';
+
+
+const contact1 = {
+    name :'Marbouille',
+    avatar: 'https://randomuser.me/api/portraits/women/17.jpg',
+    online: true
+};
+
+const Contact = function (Lina){
+    return(
+        <div className="Contact">
+        <img className="avatar" src={contact1.avatar}/> 
+        <div className="status">
+            <h2 className="name">{contact1.name}</h2>
+            <div>
+        {contact1.online ? <div className="status-online"></div> : <div className="status-offline"></div> }
+        {contact1.online ? <div className="status-text">"Online"</div> : <div className="status-text">"Offline"</div>}
+        </div>
+        </div>
+        </div>
+        )
+    }
+
+export default Contact;
